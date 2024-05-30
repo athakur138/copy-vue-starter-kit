adf681792cd581ef958bdae4509f02cf9647b7a4
athakur138 <79656636+athakur138@users.noreply.github.com>
2024-05-30 13:34:50 +0530

    Initial commit

---
+++ src/views/Test/Count.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from 'vue'
+// Create a reactive reference to store the count value
+const count = ref(0)
+
+</script>
+
+<template>
+    <h1>Hi</h1>
+  <button @click="count++">Count is: {{ count }}</button>
+</template>
+
+<style scoped>
+button {
+  font-weight: bold;
+}
+</style>
